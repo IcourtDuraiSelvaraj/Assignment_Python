5f6d192653d80c8480fb4512b6d185affe848cc1
IcourtDuraiSelvaraj <51900552+IcourtDuraiSelvaraj@users.noreply.github.com>
2019-06-17 11:49:21 +0530

    Update EmployeeAvailability.py

--- Python_Assignment1/EmployeeEntry/EmployeeAvailability.py
+++ Python_Assignment1/EmployeeEntry/EmployeeAvailability.py
@@ -2,7 +2,9 @@ import pickle
 
 def employee_Availability(name, password):
     print(name+" "+password)
-    dbfile = open('EmployeeDB', 'rb')
+    dbfile = open('Employee_DB', 'rb')
     data = pickle.load(dbfile)
-    print(data['Name'])
+    #print(data['Name'])
+     if data['Name'] in name and data['Password'] in password:
+        print("Success")
     dbfile.close()
